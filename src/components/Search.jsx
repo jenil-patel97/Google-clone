857d14a2e35c2218ee228a72da26f06e008fc454
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-09 10:53:05 +0530

    feat : Reducer and StateProvider

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import Mic from "../assets/google-voice.png";
 import Lens from "../assets/Google-Lens.png";
 
 const Search = ({ hideBtn }) => {
+  const [input, setInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+    alert(input);
+  };
   return (
     <div>
       <div className="flex items-center rounded-full border border-zinc-400 h-12 px-[15px] py-[10px] w-[560px] mt-[20px] m-auto">
         <AiOutlineSearch color="gray" size={22} />
-        <input className="flex-1 px-[13px] py-[8px] font-medium border-none outline-none" />
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="flex-1 px-[13px] py-[8px] font-medium border-none outline-none"
+        />
         <img src={Mic} alt="mic" />
         <img src={Lens} alt="lens" width={20} className="ml-5" />
       </div>
@@ -16,6 +26,7 @@ const Search = ({ hideBtn }) => {
       {!hideBtn && (
         <div className="flex justify-center mt-[32px] space-x-6">
           <button
+            onClick={search}
             className="space-x-8 px-4 py-2 rounded-md bg-[#f5f1f1] border border-white hover:border-slate-300"
             type={"submit"}
           >
